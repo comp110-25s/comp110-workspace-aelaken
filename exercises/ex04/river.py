0cba5911a9b4341f61109951bd8cba2a49688515
aelaken <alaken@unc.edu>
2025-04-10 00:29:49 -0400

    My recent projects in COMP110 that I have been working.

--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -1,11 +1,17 @@
 """File to define River class."""
 
-from exercises.EX04.fish import Fish
-from exercises.EX04.bear import Bear
+from exercises.ex04.fish import Fish
+from exercises.ex04.bear import Bear
+
 
 class River:
-    
-    def __init__(self, num_fish: int, num_bears:int):
+
+    day: int
+    bears: list[Bear]
+    fish: list[Fish]
+    """Class to define River."""
+
+    def __init__(self, num_fish: int, num_bears: int):
         """New River with num_fish Fish and num_bears Bears"""
         self.day: int = 0
         self.fish: list[Fish] = []
@@ -16,24 +22,54 @@ class River:
         for _ in range(0, num_bears):
             self.bears.append(Bear())
 
+
     def check_ages(self):
+        """ makes sure the fish does not live past 3 and bears dont live past 5"""
+        for fish in self.fish:
+                if fish.age > 3:
+                    self.fish.remove(fish)
+
+        for bear in self.bears:
+            if bear.age>5:
+                self.bears.remove(bear)
         return None
 
     def bears_eating(self):
-        return None
-    
-    def check_hunger(self):
-        return None
-        
-    def repopulate_fish(self):
-        return None
-    
-    def repopulate_bears(self):
-        return None
-    
-    def view_river(self):
-        return None
+        """if there are at least 5 fish the bear will eat 3"""
+        for bear in self.bears:
+            if len(self.fish) >= 5:
+                bear.eat(3)
+                self.remove_fish(3)
             
+        return None
+
+    def check_hunger(self):
+        """Checks if the bear is starving and kills them"""
+        for bear in self.bears:
+            if bear.hunger_score < 0:
+                self.bears.remove(bear)
+        return None
+
+    def repopulate_fish(self) -> None:
+        """modles fish reporduction"""
+        num_new_fish = (len(self.fish) // 2) * 4
+        for fish in range(num_new_fish):
+            self.fish.append(Fish())
+        return None
+
+    def repopulate_bears(self):
+        """modles bear reporduction"""
+        num_new_bears = len(self.bears) // 2
+        for bear in range(num_new_bears):
+            self.bears.append(Bear())
+        return None
+
+    def view_river(self):-> None
+        """ tells me the status of my bears and fish in the river"""
+        print(f"~~Day {self.day}:~~")
+        print(f"Fish population: {len(self.fish)}")
+        print(f"Bears population: {len(self.bears)}")
+
     def one_river_day(self):
         """Simulate one day of life in the river"""
         # Increase day by 1
@@ -56,4 +92,18 @@ class River:
         self.repopulate_bears()
         # Visualize River
         self.view_river()
-            
+
+
+    def one_river_week(self):
+        """Tells me about the river in one week"""
+        for _ in range(0, 7):
+            self.one_river_day()
+        return None
+
+
+
+def remove_fish(self, amount: int) -> None:
+    """should remove the frontmost fish in the list/river"""
+    for x in range(amount):
+        if len(self.fish) > 0:
+            self.fish.pop(0)
--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -1,1 +1,10 @@
+from exercises.ex04.fish import Fish
+from exercises.ex04.bear import Bear
+from exercises.ex04.river import River
 
+
+my_river = River(10, 2)
+
+
+my_river.view_river()
+my_river.one_river_week()
